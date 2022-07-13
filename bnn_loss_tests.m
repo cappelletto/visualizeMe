ce4992aaffa9e0bea5ac59149a8063dcecb920b6
cappelletto <cappelletto@gmail.com>
2022-07-13 11:46:47 +0100

    Auto select plot settings depending of training or validation mode. Add dataset_key verification, onlye accepted 'valid' and 'train'. Update title string to reflect current dataset

--- bnn_loss_tests.m
+++ bnn_loss_tests.m
@@ -11,7 +11,7 @@
 
 % Retrieve and process training / validation results from trained BNN predictor
 
-function bnn_loss_test(folder_path, dataset)
+function bnn_loss_test(folder_path, dataset_key)
 
 % Check if folder_path is not defined
 if ~exist('folder_path', 'var')
@@ -20,19 +20,26 @@ if ~exist('folder_path', 'var')
 end
 
 % Check if dataset = train/valid option defined. Default analyze validation
-if ~exist ('dataset', 'var') 
+if ~exist ('dataset_key', 'var') 
     warning ("Using default dataset subset: validation")
     dataset_key = 'valid'
+else % it as defined by user, let's validate it
+    if (strcmp (dataset_key, 'train'))
+        print ("Processing a Training dataset")
+    elseif (strcmp (dataset_key, 'valid'))
+        print ("Processing a Validation dataset")
+    else
+        print (strcat("Error, unknown dataset_key provided: ", dataset_key))
+        return
+    end
 end
 
 % Retrieve the list of files in the folder that correspond to the subset
 files = dir (strcat(folder_path, strcat (dataset_key, '_*.csv')));
 
 K = length(files)   % Number of files
-% Use all the rows in the train/validation output for the last exported
-% training epoch
-
-% Create empty vector to store the mean values of train/validation fit and kld loss functions
+% Use all the rows in the train/validation output for the last exported training epoch
+% Create empty vector to store the mean values of train/validation fit (RMSE)
 mean_train_fit = zeros(K,1);
 mean_valid_fit = zeros(K,1);
 length_meter   = zeros(K,1);
@@ -46,10 +53,6 @@ for i = 1:K
     fln = files(i).name(15:end-9);
     length_meter(i) = str2double(fln); % it will be in L<length_meter>m.csv format
 
-    % target    = data.target_slope_log;
-    % predicted = data.pred_slope_log;
-    % target    = data.target_mean_slope;
-    % predicted = data.pred_mean_slope;
     % TODO: Use variable name to determine which column is target/pred
     target    = data(:,1);  % First column is the target value
     predicted = data(:,2);  % Second column is the predicted value
@@ -79,23 +82,28 @@ length_meter   = data_sorted(:,1);
 mean_train_fit = data_sorted(:,2);
 mean_valid_fit = data_sorted(:,3);
 
-% TODO: Determine from the input filelist if dataset corresponds to training or validation (colour/label accordingly)
+% TODO: Extract the folder path name with the experiment ID and add it to the Title string
+
+if (strfind(dataset_key, 'train') > 0)
+    edgeColor = [0 0.4470 0.7410]; % Blue-ish
+    YLabelString = 'BNN - Prediction error (RMSE) for Trainng dataset'
+    titleLabelString = 'BNN - Training loss vs distance parameter'
+else % we should check it is 'valid' otherwise is an error (invalid key defined)
+    edgeColor = [0.8500 0.3250 0.0980]; % Red-ish
+    YLabelString = 'BNN - Prediction error (RMSE) for Validation dataset'
+    titleLabelString = 'BNN - Validation loss vs distance parameter'
+end
 
 % Before plotting, we create an empty figure
 figure; hold on; grid on;
 % Plot the loss function. X = length_meter, Y = loss
-scatter(length_meter, mean_train_fit, 'b', 'LineWidth', 2, 'MarkerEdgeAlpha', 0.3);
-%plot(length_meter, mean_train_fit, 'b', 'LineWidth', 2);
-% scatter(length_meter, mean_train_kld, 'r', 'LineWidth', 2, 'MarkerEdgeAlpha', 0.3);
-%plot(length_meter, mean_train_kld, 'r', 'LineWidth', 2);
-% plot(length_meter, mean_train_fit + mean_train_kld, 'b', 'LineWidth', 2);
-% ylim([0, max(mean_train_kld)+0.02]);
+scatter(length_meter, mean_train_fit, 'b', 'LineWidth', 2, 'MarkerEdgeAlpha', 0.3, MarkerEdgeColor, edgeColor);
 xlabel('Distance parameter (m)', 'FontSize', 16);
-ylabel('BNN - Training loss', 'FontSize', 16);
+ylabel( YLabelString, 'FontSize', 16);
 legend ('Fitting loss', "", 'KL-divergence loss', "");
 %legend ('Fitting loss', 'KL-divergence loss', "Total loss");
 ylim([0 max(mean_train_fit)*1.05])
-title ("BNN train loss vs sigma parameter. h16 - 1E6 - MID Network. ELBO = 10.0")
+title (titleLabelString)
 
 % Before plotting, we create an empty figure
 figure; hold on; grid on;
